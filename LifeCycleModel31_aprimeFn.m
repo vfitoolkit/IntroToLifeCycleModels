ca338a7a70b3043c180c25693a95318b74168681
robertdkirkby <robertdkirkby@gmail.com>
2023-09-11 09:03:48 +1200

    relocate portfolio

--- LifeCycleModel31_aprimeFn.m
+++
@@ -1,8 +0,0 @@
-function aprime=LifeCycleModel31_aprimeFn(savings,riskyshare,u, r)
-% Note: because of how Case3 works we need to input (d1,d2,u) as the first arguements.
-% That is, the first inputs must be the decision variables (d variables),
-% followed by the shocks that are iid and occur between periods (u variables)
-
-aprime=(1+r)*(1-riskyshare)*savings+(1+r+u)*riskyshare*savings;
-
-end
